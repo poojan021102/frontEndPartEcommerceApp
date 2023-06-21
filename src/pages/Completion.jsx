846f2b3d9f3bc72cea6f3a59e63eb53c35291012
Poojan Patel <poojanpatel02112002@gmail.com>
2023-06-21 11:09:15 +0530

    Added completion page

--- src/pages/Completion.jsx
+++ src/pages/Completion.jsx
@@ -1,5 +1,18 @@
-function Completion(props) {
-    return <h1>Thank you! 🎉</h1>;
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { checkIfUserAlreadyExists } from "../redux/actions";
+import { Link } from "react-router-dom";
+function Completion() {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+      dispatch(checkIfUserAlreadyExists());
+    },[]);
+    return (
+    <div style={{display:"flex", flexDirection:"column",justifyContent:"center",alignItems:"center", padding:"20px"}}>
+      <h1>Thank you! 🎉 Your order has been placed and will be delivered soon</h1>;
+      <Link to = "/" className = "btn btn-danger px-5 py-2 mb-5">Shop More</Link>
+    </div>
+    )
   }
   
   export default Completion;
